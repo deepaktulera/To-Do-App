1b93ae41d6ef44a60e538a287622c3d753df978c
deepaktulera <deepakstuleraoo3@gmail.com>
2026-05-19 14:40:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
+import ToDoList from './components/ToDoList';
 
 const App = () => {
+  const [task, setTask] = useState('');
+
+  const [todo, setTodo] = useState([]);
+    
+
+  const addTask = () => {
+    if (task.trim() === '') return;
+    const newTodo = {
+      id: Date.now(),
+      text: task,
+      completed: false
+    }
+
+    setTodo([...todo, newTodo]);
+    setTask('')
+  }
+  
   return (
     <div>
       <Header />
+      <div className='flex justify-center pt-6 px-4 gap-2'>
+        <input
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          type="text" placeholder='Enter your task' className='border p-2 px-4 rounded-full w-full sm:w-[70%]' />
+        <button
+          onClick={addTask}
+          className='bg-black text-white px-4 sm:px-6 rounded-full'>Add</button>
+      </div>
+      <ToDoList todo={todo} />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Header = () => {
   return (
     <nav>
-      <h1 className='text-2xl sm:text-4xl text-center p-2'>To-Do App</h1>
+      <h1 className="
+      text-3xl sm:text-4xl font-extrabold text-center p-2
+      bg-linear-to-r from-pink-300 to-blue-400
+      bg-cover bg-center
+      bg-clip-text text-transparent
+    "
+  >To-Do App</h1>
     </nav>
   );
 }
