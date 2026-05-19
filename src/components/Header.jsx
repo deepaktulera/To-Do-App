fc6938931ae36f874914ba9b6f44b0d98ea41f51
deepaktulera <deepakstuleraoo3@gmail.com>
2026-05-19 09:50:19 +0530

    Create components and import tailwindcss

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav>
+      <h1 className='text-2xl sm:text-4xl text-center p-2'>To-Do App</h1>
+    </nav>
+  );
+}
+
+export default Header;
