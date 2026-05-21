7ccb001d6d47a09e4d61ab763534a0f21ebb38c4
deepaktulera <deepakstuleraoo3@gmail.com>
2026-05-21 19:42:42 +0530

    Update and Style App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,84 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from './components/Header';
-import ToDoList from './components/ToDoList';
+import ToDoList from './components/ToDoList'
+import { useState } from 'react';
 
 const App = () => {
-  const [task, setTask] = useState('');
+  const [task, settask] = useState('');
 
-  const [todo, setTodo] = useState([]);
-    
+  const [Tasks, setTasks] = useState([]);
 
-  const addTask = () => {
-    if (task.trim() === '') return;
-    const newTodo = {
+  function handleAdd() {
+    if (task.trim() == '') return;
+    const newToDo = {
       id: Date.now(),
       text: task,
       completed: false
     }
-
-    setTodo([...todo, newTodo]);
-    setTask('')
+    setTasks([...Tasks, newToDo])
+    settask('')
   }
-  
+
+  function handleComplete(id) {
+
+    const updateTask = Tasks.map((item) => {
+
+      if (item.id === id) {
+        return { ...item, completed: !item.completed }
+      }
+      return item;
+    });
+
+    setTasks(updateTask);
+  }
+
+  function handleDelete(id) {
+
+    const filterTask = Tasks.filter((item) => {
+      return item.id !== id
+    });
+
+    setTasks(filterTask);
+  }
+
+  function handleEdit(id, text) {
+  settask(text);
+  const updatedTasks = Tasks.filter((item) => {
+    return item.id !== id;
+  });
+
+  setTasks(updatedTasks);
+}
+
   return (
     <div>
       <Header />
-      <div className='flex justify-center pt-6 px-4 gap-2'>
+      <div className='flex w-full gap-2 justify-center'>
+
         <input
+          className='w-[70%] border outline-none rounded-full p-2'
           value={task}
-          onChange={(e) => setTask(e.target.value)}
-          type="text" placeholder='Enter your task' className='border p-2 px-4 rounded-full w-full sm:w-[70%]' />
+          placeholder='Enter task ....'
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleAdd()
+            }
+          }}
+          onChange={(e) => {
+            settask(e.target.value)
+          }}
+          type="text" />
         <button
-          onClick={addTask}
-          className='bg-black text-white px-4 sm:px-6 rounded-full'>Add</button>
+          className='bg-sky-300 p-3 rounded-full active:scale-90'
+          type='submit' onClick={handleAdd}>Add</button>
       </div>
-      <ToDoList todo={todo} />
+      {task.length === 0 && (
+        <p className='text-gray-400 p-4'>
+          No tasks available
+        </p>
+      )}
+      <ToDoList task={Tasks} complete={handleComplete} deleteTask={handleDelete}  editTask={handleEdit} />
+      <button></button>
     </div>
   );
 }
